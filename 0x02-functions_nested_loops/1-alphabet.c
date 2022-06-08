b372111f62d96261509ed5771cd40104f9beb98b
isiossai <isiomaossai@icloud.com>
2022-06-08 01:32:25 +0100

    0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * Description: function that prints the alphabet, in lowercase, followed by a new line.
+ * Description: function that prints the alphabet, in lowercase.
  * Return: Always 0 (Success)
  */
 
