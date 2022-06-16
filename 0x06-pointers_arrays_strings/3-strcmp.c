8a2068f59e39125edd04dd556cc8d8713f18cef3
isiossai <isiomaossai@icloud.com>
2022-06-16 17:27:23 +0100

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,26 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if matching, and ns1 - ns2 if not matching
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+int i;
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-char s1[] = "Hello";
-char s2[] = "World!";
+ * We have reached the end of S1 OR we have reached the end of S2
+ * 
+*/
 
-printf("%d\n", _strcmp(s1, s2));
-printf("%d\n", _strcmp(s2, s1));
-printf("%d\n", _strcmp(s1, s1));
+for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+{
+	if (s1[i] != s2[i])
+	{
+		return (s1[i] - s2[i]);
+	}
+}
 return (0);
 }
